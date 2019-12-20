36cd7194c2e4aa1076c4bfef9328d5540b2eb7a2
yaphet <darion.wang@vesoft.com>
2019-12-20 15:23:03 +0800

    fix style (#44)

--- client/src/main/java/com/vesoft/nebula/client/storage/processor/Processor.java
+++ client/src/main/java/com/vesoft/nebula/client/storage/processor/Processor.java
@@ -3,6 +3,7 @@
  * This source code is licensed under Apache 2.0 License,
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
+
 package com.vesoft.nebula.client.storage.processor;
 
 public interface Processor<T> {
