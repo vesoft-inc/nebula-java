713fe8e2eda9ddc4cbb254df0249a5e56ca09a7a
huangzhaolai-jk <huangzhaolai-jk@360jinrong.net>
2020-03-23 11:34:03 +0800

    Revert "fix new package"
    This reverts commit 33a77ad35283534dc342be0832c6044ed43dd38a.

---
+++ examples/src/main/java/com/vesoft/nebula/examples/database/TestConnect.java
@@ -0,0 +1,36 @@
+package com.vesoft.nebula.examples.database;
+
+import com.vesoft.nebula.database.NebulaDataSource;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author huangzhaolai-jk
+ * @version 1.0.0
+ * @Description TestConnect is used for
+ * @Date 2020/3/23 - 10:35
+ */
+public class TestConnect implements ApplicationRunner {
+
+    public static void main(String[] args){
+
+    }
+
+    @Resource
+    private NebulaDataSource nebulaDataSource;
+
+    /**
+     * to run ExampleApplication than auto test this method
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("启动中！");
+        for (int i = 0; i < 100; i++) {
+            new Thread(new NebulaDatabasePoolTestTh(nebulaDataSource,i)).start();
+        }
+    }
+}
