8a95c3419481ab7ad40860290cce6c0bea0ea3a5
Mengjie <limengjie@hotmail.com>
2020-07-17 16:02:51 +0800

    Add spark importer's scripts (#111)
    * Add spark importer's scripts

---
+++ tools/spark-importer/scripts/json2neo4j.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+# --coding:utf-8--
+
+# Copyright (c) 2019 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
+
+from neo4j import GraphDatabase
+import json
+import threading
+
+edge_batch_size = 1000
+vertex_batch_size = 1000
+
+
+def dict2json(data: dict):
+    parse_str = ''
+    for key, value in data.items():
+        parse_str += ',' + key + ":" + \
+                     ('"' + str(value) + '"' if isinstance(value, str) else str(value)) + " "
+    return "{ " + parse_str[1:] + "}"
+
+
+def addVertex(tx, element: dict, name: str):
+    for data in element:
+        statement = "create (:{} {})".format(
+            name, dict2json(data))
+        tx.run(statement)
+
+
+def addEdge(tx, element: dict, name: str):
+    for data in element:
+        from_statement = "(a:{} {})".format(
+            data["from"]["tag"], dict2json(data["from"]["match"]))
+        to_statement = "(b:{} {})".format(
+            data["to"]["tag"], dict2json(data["to"]["match"]))
+        edge_statement = '[:{} {}]'.format(
+            name, dict2json(data["data"]))
+        statement = "match {} match {} create (a)-{}->(b)".format(
+            from_statement, to_statement, edge_statement)
+        tx.run(statement)
+
+
+def runAddVertex(session, name: str, data: list):
+    print('start thread:', threading.currentThread().getName())
+    vertext_time = time.time()
+    ids = 0
+    batch_ids = 0
+    total_count = len(data)
+    while ids < total_count:
+        batch_data = data[ids:ids + vertex_batch_size]
+        batch_start_time = time.time()
+        session.write_transaction(
+            addVertex, batch_data, name)
+        ids += vertex_batch_size
+        batch_ids += 1
+        print("%{}:vertex write {}/{} use {} seconds".format(batch_ids,
+                                                             ids, total_count, time.time() - batch_start_time))
+    vertex_use_time = {
+        "tagname": name,
+        "use_time": time.time() - vertext_time,
+        "property_keys": [] if len(data) == 0 else list(data[0].keys()),
+        "length": len(v['data'])
+    }
+    print(vertex_use_time)
+
+
+def runAddEdge(session, name: str, data: list):
+    edge_time = time.time()
+    ids = 0
+    batch_ids = 0
+    total_count = len(data)
+    while ids < total_count:
+        batch_data = data[ids:ids + edge_batch_size]
+        batch_start_time = time.time()
+        session.write_transaction(
+            addEdge, batch_data, name)
+        ids += edge_batch_size
+        batch_ids += 1
+        print("%{}:edge write {}/{} use {} seconds".format(batch_ids,
+                                                           ids, total_count, time.time() - batch_start_time))
+
+    edge_use_time = {
+        "edgename": name,
+        "use_time": time.time() - edge_time,
+        "property_kest": [] if len(data) == 0 else list(data[0].keys()),
+        "length": len(data)
+    }
+    print(edge_use_time)
+
+
+if __name__ == '__main__':
+
+    import argparse
+    import time
+
+    parser = argparse.ArgumentParser(description='import json file to neo4j')
+
+    parser.add_argument('-f', '--file', help='json file path',
+                        type=str, required=True)
+    parser.add_argument('-a', '--address', help='neo4j address',
+                        type=str, default="bolt://127.0.0.1:7687")
+    parser.add_argument('-p', '--password',
+                        help='neo4j password', type=str, default="neo4j")
+    parser.add_argument('-u', '--user', help='neo4j user name',
+                        type=str, default="neo4j")
+    parser.add_argument('-t', '--thread_num', type=int, default=4)
+    args = parser.parse_args()
+
+    time_report = {"vertex_use_time": [], "edge_use_time": [],
+                   "vertex_batch_size": vertex_batch_size, "edge_batch_size": edge_batch_size}
+    vertex_use_time = time_report["vertex_use_time"]
+    edge_use_time = time_report["edge_use_time"]
+
+    thread_num = args.thread_num
+    driver = GraphDatabase.driver(
+        args.address, auth=(args.user, args.password))
+    with open(args.file) as f:
+        data = json.load(f)
+        print("start write")
+        sessions = [driver.session() for i in range(thread_num)]
+        total_start_time = time.time()
+        time_report["total_start_time"] = total_start_time
+
+        thread_vertex = []
+        tag_count = len(data['vertex'])
+        thread_tag_count = thread_num // tag_count
+        if thread_tag_count < 1:
+            raise Exception("thread_num must great tag_count")
+
+        vertices_time = time.time()
+        session_ids = 0
+        for name, v in data["vertex"].items():
+            vertex_count = len(v['data'])
+            per_thread_tag_count = vertex_count // thread_tag_count
+            for i in range(thread_tag_count):
+                end = (i + 1) * per_thread_tag_count if i + \
+                                                        1 < thread_tag_count else vertex_count
+                t = threading.Thread(target=runAddVertex, name='thread-' + name, args=(
+                    sessions[session_ids], name, v['data'][i * per_thread_tag_count:end]))
+                t.start()
+                session_ids += 1
+                thread_vertex.append(t)
+        for t in thread_vertex:
+            t.join()
+
+        time_report["total_vertex"] = {
+            "vertex_num": len(data["vertex"]),
+            "use_time": time.time() - vertices_time
+        }
+
+        session_ids = 0
+        thread_edge = []
+        edge_type_count = len(data['edge'])
+        thread_edge_type_count = thread_num // edge_type_count
+        if thread_edge_type_count < 1:
+            raise Exception("thread_num must great edge_type_count")
+        edges_time = time.time()
+        for name, e in data["edge"].items():
+            edge_count = len(e['data'])
+            per_thread_edge_count = edge_count // thread_edge_type_count
+            for i in range(thread_edge_type_count):
+                end = (i + 1) * per_thread_edge_count if i + 1 < thread_edge_type_count else edge_count
+                t = threading.Thread(target=runAddEdge, name='thread-' + name, args=(
+                    sessions[session_ids], name, e['data'][i * per_thread_edge_count:end]
+                ))
+                t.start()
+                session_ids += 1
+                thread_vertex.append(t)
+        for t in thread_edge:
+            t.join()
+
+        time_report["total_edge"] = {
+            "edge_num": len(data["edge"]),
+            "use_time": time.time() - edges_time
+        }
+        time_report["begin_to_end_insert"] = time.time() - total_start_time
+        map(lambda session: session.close(), sessions)
+    driver.close()
+    print(json.dumps(time_report, indent=4))
---
+++ tools/spark-importer/scripts/verify_nebula.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+# --coding:utf-8--
+
+# Copyright (c) 2019 vesoft inc. All rights reserved.
+#
+# This source code is licensed under Apache 2.0 License,
+# attached with Common Clause Condition 1.0, found in the LICENSES directory.
+
+
+from nebula.ConnectionPool import ConnectionPool
+from nebula.Client import GraphClient
+from nebula.Common import *
+from graph import ttypes
+
+from pyhocon import ConfigFactory
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s-%(levelname)s-%(message)s')
+logger = logging.getLogger(__name__)
+
+
+def do_simple_execute(client, cmd):
+    print("do execute %s" % cmd)
+    resp = client.execute(cmd)
+    if resp.error_code != 0:
+        print('Execute failed: %s, error msg: %s' % (cmd, resp.error_msg))
+        raise ExecutionException(
+            'Execute failed: %s, error msg: %s' % (cmd, resp.error_msg))
+
+
+def fetch_info(client: GraphClient, key: str):
+    return client.execute_query("FETCH PROP ON " + key)
+
+
+def handle_fetch_resp(resp):
+    if not resp.rows:
+        return []
+    ret = []
+    for row in resp.rows:
+        if len(resp.column_names) != len(row.columns):
+            continue
+        name_value_list = {}
+        for name, col in zip(resp.column_names, row.columns):
+            name = str(name, encoding='utf8')
+            if '.' in name:
+                name = name.split('.')[1]
+
+            if col.getType() == ttypes.ColumnValue.__EMPTY__:
+                print('ERROR: type is empty')
+            elif col.getType() == ttypes.ColumnValue.BOOL_VAL:
+                name_value_list[name] = col.get_bool_val()
+            elif col.getType() == ttypes.ColumnValue.INTEGER:
+                name_value_list[name] = col.get_integer()
+            elif col.getType() == ttypes.ColumnValue.ID:
+                name_value_list[name] = col.get_id()
+            elif col.getType() == ttypes.ColumnValue.STR:
+                name_value_list[name] = col.get_str().decode('utf-8')
+            elif col.getType() == ttypes.ColumnValue.DOUBLE_PRECISION:
+                name_value_list[name] = col.get_double_precision()
+            elif col.getType() == ttypes.ColumnValue.TIMESTAMP:
+                name_value_list[name] = col.get_timestamp()
+            else:
+                print('ERROR: Type unsupported')
+        ret.append(name_value_list)
+    return ret
+
+
+class TagConf:
+    def __init__(self, tag_name: str, id_field: str, id_policy: str, fields: list, type_name=""):
+        self.tag_name = tag_name
+        self.id_field = id_field
+        self.id_policy = id_policy
+        self.fields = fields
+        self.type_name = type_name
+
+    def __repr__(self):
+        return self.tag_name
+
+    def get_id(self, tag_id, fields: list):
+        ret = (tag_id if self.id_policy == '' else self.id_policy + '({})'.format(tag_id)) + ":"
+        ret += "(" + ','.join([fields[field] for field in self.fields]) + ")"
+        return ret
+
+
+class EdgeConf:
+    def __init__(self, edge_name, from_field: str, from_policy: str, to_field: str, to_policy: str, fields: list,
+                 ranking='', type_name=""):
+        self.edge_name = edge_name
+        self.from_field = from_field
+        self.from_policy = from_policy
+        self.to_field = to_field
+        self.to_policy = to_policy
+        self.fields = fields
+        self.type_name = type_name
+        self.ranking = ranking
+
+    def __repr__(self):
+        return self.edge_name
+
+    def get_id(self, from_id, to_id, ranking=0):
+        return (str(from_id) if self.from_policy == '' else self.from_policy + '({})'.format(from_id)) + "->" + \
+               (str(to_id) if self.to_policy == '' else self.to_policy + '({})'.format(to_id)) + '@{}'.format(ranking)
+
+
+class HandleConf:
+
+    def __init__(self, conf):
+        self.tag_data = {}
+        self.edge_data = {}
+        for c in conf["tags"]:
+            tag_name = c.get_string("name")
+            type_name = c.get_string("type", "")
+            if "vertex.field" in c:
+                id_field = c.get_string("vertex.field")
+                id_policy = c.get_string("vertex.policy", "")
+            else:
+                id_field = c.get_string("vertex")
+                id_policy = ""
+            fields = list(map(lambda x: str(x), c.get("fields").values()))
+            self.tag_data[tag_name] = TagConf(
+                tag_name, id_field, id_policy, fields, type_name)
+
+        for c in conf["edges"]:
+            edge_name = c.get_string("name")
+            type_name = c.get_string("type", "")
+            if "source.field" in c:
+                from_field = c.get_string("source.field")
+                from_policy = c.get_string("source.policy", "")
+            else:
+                from_field = c.get_string("source")
+                from_policy = ""
+            if "target.field" in c:
+                to_field = c.get_string("target.field")
+                to_policy = c.get_string("target.policy", "")
+            else:
+                to_field = c.get_string("target")
+                to_policy = ""
+            ranking = c.get_string("ranking", "")
+            fields = list(map(lambda x: str(x), c.get("fields").values()))
+            self.edge_data[edge_name] = EdgeConf(
+                edge_name, from_field, from_policy, to_field, to_policy, fields, ranking, type_name)
+
+
+def verify_nebula(client: GraphClient, datas: list, conf):
+    nebula_space = conf.get_string("nebula.space")
+    do_simple_execute(client, 'use ' + nebula_space)
+    batch_size = 50
+    full_conf = HandleConf(conf)
+    json_vertex_data = {}
+    json_edge_data = {}
+    for data in datas:
+        for tag_name, vertices in data["vertex"].items():
+            json_vertex_data[tag_name] = vertices["data"]
+        for edge_name, edge in data["edge"].items():
+            json_edge_data[edge_name] = edge["data"]
+    for tag_name, tag_conf in full_conf.tag_data.items():
+        logger.info("verify tag:" + tag_name)
+        data = json_vertex_data[tag_name]
+        query = 'fetch prop on {} '.format(tag_name)
+        ids = 0
+        while ids < len(data):
+            _ = 0
+            batch_ids = []
+            search_map = {}
+            while _ < batch_size and ids < len(data):
+                batch_ids.append(str(data[ids][tag_conf.id_field]))
+                search_map[str(data[ids][tag_conf.id_field])] = data[ids]
+                ids += 1
+                _ += 1
+            exec_query = query + ','.join(batch_ids)
+            logger.info("nebula exec: " + exec_query)
+
+            query_resp = client.execute_query(exec_query)
+            if query_resp.error_code != 0:
+                logger.error("resp msg:" + query_resp.error_msg)
+            else:
+                nebula_datas = handle_fetch_resp(query_resp)
+                if len(batch_ids) != len(nebula_datas):
+                    logger.error("search length:{}, real length:{}".format(
+                        len(batch_ids), len(nebula_datas)))
+                for nebula_data in nebula_datas:
+                    json_data = search_map[str(
+                        nebula_data[str(tag_conf.id_field)])]
+                    for field in tag_conf.fields:
+                        if nebula_data[field] != json_data[field]:
+                            logger.error("json data:{} , nebula data:{} , field:{}, json value:{}, nebula value: {}",
+                                         json_data, nebula_data, field, json_data[field], nebula_data[field])
+
+    def trim_edge_id(x):
+        return x if '.' not in x else x.split('.')[1]
+
+    for edge_name, edge_conf in full_conf.edge_data.items():
+        logger.info("verify edge:" + edge_name)
+        query = 'fetch prop on {} '.format(edge_name)
+        data = json_edge_data[edge_name]
+        for json_data in data:
+
+            edge_id = edge_conf.get_id(json_data['from']['match'][trim_edge_id(edge_conf.from_field)],
+                                       json_data['to']['match'][trim_edge_id(edge_conf.to_field)],
+                                       0 if edge_conf.ranking == '' else json_data['data'][edge_conf.ranking]
+                                       )
+
+            exec_query = query + edge_id
+            logger.info("nebula exec: " + exec_query)
+
+            query_resp = client.execute_query(exec_query)
+            if query_resp.error_code != 0:
+                logger.error("resp msg: " + query_resp.error_msg)
+            else:
+                nebula_datas = handle_fetch_resp(query_resp)
+                if len(nebula_datas) != 1:
+                    logger.error("nebula data:{}, json_data:{}".format(
+                        nebula_datas, json_data))
+                else:
+                    nebula_data = nebula_datas[0]
+                    for field in edge_conf.fields:
+                        if field not in nebula_data:
+                            logger.error(
+                                "field:{} not in nebula data{}", field, nebula_data)
+                        if nebula_data[field] != json_data['data'][field]:
+                            logger.error("json data:{} , nebula data:{} , field:{}, json value:{}, nebula value: {}",
+                                         json_data, nebula_data, field, json_data[field], nebula_data[field])
+                    if edge_conf.ranking != '':
+                        if nebula_data['_rank'] != json_data['data'][edge_conf.ranking]:
+                            logger.error("rank field {}, nebula rank {}, json rank {}".format(edge_conf.ranking,
+                                                                                              nebula_data['_rank'],
+                                                                                              json_data['data'][
+                                                                                                  edge_conf.ranking]))
+
+
+if __name__ == '__main__':
+    import argparse
+    import json
+
+    parser = argparse.ArgumentParser(description='verify nebula use json')
+
+    parser.add_argument('-j', '--jsons', help='json files path',
+                        nargs='+', type=str, required=True)
+    parser.add_argument('-a', '--address', help='nebula address',
+                        type=str, default="127.0.0.1:3699")
+    parser.add_argument('-p', '--password',
+                        help='nebula password', type=str, default="password")
+    parser.add_argument(
+        '-u', '--user', help='nebula user name', type=str, default="user")
+
+    parser.add_argument('-c', '--config', help='spark importer config path',
+                        type=str, default="application.conf")
+
+    parser.add_argument('-l', '--log_level', help='log level=> 10:debug, 20:info, 30:warn, 40:error', type=int,
+                        default=30)
+    args = parser.parse_args()
+
+    connection_pool = ConnectionPool(
+        args.address.split(":")[0], args.address.split(":")[1])
+    client = GraphClient(connection_pool)
+    auth_resp = client.authenticate(args.user, args.password)
+    if auth_resp.error_code:
+        raise AuthException("Auth failed")
+
+    conf = ConfigFactory.parse_file(args.config)
+
+    data = []
+    for j in args.jsons:
+        with open(j) as f:
+            data.append(json.load(f))
+
+    logger.setLevel(args.log_level)
+    verify_nebula(client, data, conf)
