53b8161876fb746dc3bddfaa133df472dd8a6dd8
Mengjie <limengjie@hotmail.com>
2020-07-31 16:36:18 +0800

    Rows support string index (#120)
    * scripts add mock csv support
    
    * support string index in rows

--- client/src/main/java/com/vesoft/nebula/client/graph/ResultSet.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/ResultSet.java
@@ -7,17 +7,81 @@
 package com.vesoft.nebula.client.graph;
 
 import com.google.common.collect.Lists;
+import com.vesoft.nebula.graph.ColumnValue;
+import com.vesoft.nebula.graph.DateTime;
 import com.vesoft.nebula.graph.RowValue;
-import java.util.Iterator;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
  */
-public class ResultSet implements Iterator {
+public class ResultSet implements Serializable, Cloneable{
 
     private List<String> columns;
     private List<RowValue> rows;
+    private List<Result> results;
+
+    public static class Result implements Serializable, Comparable<Result>, Cloneable {
+        private final RowValue row;
+        private final List<String> columns;
+
+        public Result(List<String> columns, RowValue row) {
+            this.row = row;
+            this.columns = columns;
+        }
+
+        public ColumnValue get(int index) {
+            return this.row.columns.get(index);
+        }
+
+        public ColumnValue get(String key) {
+            return this.row.columns.get(columns.indexOf(key));
+        }
+
+        public String getString(String key) {
+            return new String(this.get(key).getStr()).intern();
+        }
+
+        public long getInteger(String key){
+            return this.get(key).getInteger();
+        }
+
+        public long getId(String key){
+            return this.get(key).getId();
+        }
+
+        public double getDouble(String key){
+            return this.get(key).getDouble_precision();
+        }
+
+        public float getFloat(String key){
+            return this.get(key).getSingle_precision();
+        }
+
+        public DateTime getDateTime(String key){
+            return this.get(key).getDatetime();
+        }
+
+        public boolean getBoolean(String key){
+            return this.get(key).isBool_val();
+        }
+
+        public int size() {
+            return row.columns.size();
+        }
+
+        public boolean contains(String key) {
+            return columns.contains(key);
+        }
+
+        @Override
+        public int compareTo(Result o) {
+            return this.row.compareTo(o.row);
+        }
+    }
 
     /**
      * Constructor
@@ -39,6 +103,10 @@ public class ResultSet implements Iterator {
             this.columns.add(new String(column).intern());
         }
         this.rows = rows;
+        this.results = new ArrayList<>(rows.size());
+        for (RowValue row : this.rows) {
+            this.results.add(new Result(this.columns, row));
+        }
     }
 
     /**
@@ -54,19 +122,8 @@ public class ResultSet implements Iterator {
         return this.rows;
     }
 
-    @Override
-    public boolean hasNext() {
-        return false;
-    }
-
-    @Override
-    public Object next() {
-        return null;
-    }
-
-    @Override
-    public void remove() {
-
+    public List<Result> getResults() {
+        return this.results;
     }
 
     @Override
