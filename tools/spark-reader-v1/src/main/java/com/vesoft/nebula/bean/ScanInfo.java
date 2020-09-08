2eaf3344a840a55a064c6d3ccbe03b10f307ec5a
firepation <firepation@gmail.com>
2020-09-08 14:55:18 +0800

    Spark reader v1 (#131)
    * implement spark reade nebula

---
+++ tools/spark-reader-v1/src/main/java/com/vesoft/nebula/bean/ScanInfo.java
@@ -0,0 +1,74 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+package com.vesoft.nebula.bean;
+
+import com.google.common.base.Preconditions;
+import com.vesoft.nebula.common.Checkable;
+import com.vesoft.nebula.common.Type;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class ScanInfo implements Checkable, Serializable {
+
+    private String scanType;
+
+    private String returnColString;
+
+    private int partitionNumber;
+
+    private static final String RETURN_COL_REGEX = "(\\w+=(\\w+)(,\\w+)*)(;\\w+=(\\w+)(,\\w+)*)*";
+
+    /**
+     * @param scanType    scan element type
+     * @param returnColString  return col string example: labela=name,age;labelb=name
+     */
+    public ScanInfo(String scanType, String returnColString, int partitionNumber) {
+        this.scanType = scanType;
+        this.returnColString = returnColString;
+        this.partitionNumber = partitionNumber;
+    }
+
+    @Override
+    public void check() throws IllegalArgumentException {
+        boolean isLegalType = Type.VERTEX.getType().equalsIgnoreCase(scanType)
+                || Type.EDGE.getType().equalsIgnoreCase(scanType);
+        Preconditions.checkArgument(isLegalType,
+                "scan type '%s' is illegal, it should be '%s' or '%s'",
+                scanType, Type.VERTEX.getType(), Type.EDGE.getType());
+
+        boolean isReturnColLegal = Pattern.matches(RETURN_COL_REGEX, returnColString);
+        Preconditions.checkArgument(isReturnColLegal,
+                "return col string '%s' is illegal, the pattern should like a=b,c;d=e",
+                returnColString);
+    }
+
+    public String getScanType() {
+        return scanType;
+    }
+
+    public int getPartitionNumber() {
+        return partitionNumber;
+    }
+
+    public Map<String, List<String>> getReturnColMap() {
+        check();
+
+        Map<String, List<String>> result = new HashMap<>();
+        String[] returnColSplits = returnColString.split(";");
+        for (String returnColSplit : returnColSplits) {
+            String[] labelPropertyMap = returnColSplit.split("=");
+            String label = labelPropertyMap[0];
+            List<String> properties = Arrays.asList(labelPropertyMap[1].split(","));
+            result.put(label, properties);
+        }
+        return result;
+    }
+}
---
+++ tools/spark-reader-v1/src/main/java/com/vesoft/nebula/reader/ScanEdgeIterator.java
@@ -0,0 +1,93 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
+
+package com.vesoft.nebula.reader;
+
+import com.vesoft.nebula.bean.ConnectInfo;
+import com.vesoft.nebula.bean.ScanInfo;
+import com.vesoft.nebula.client.storage.processor.ScanEdgeProcessor;
+import com.vesoft.nebula.data.Property;
+import com.vesoft.nebula.data.Result;
+import com.vesoft.nebula.storage.ScanEdgeResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.apache.spark.Partition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScanEdgeIterator extends AbstractNebulaIterator {
+
+    private Logger logger = LoggerFactory.getLogger(ScanEdgeIterator.class);
+
+    private Iterator<ScanEdgeResponse> responseIterator;
+
+    public ScanEdgeIterator(ConnectInfo connectInfo, Partition split,
+                            ScanInfo scanInfo, Map<String, Integer> propIndexMap) {
+        super(connectInfo, split, scanInfo, propIndexMap);
+        processor = new ScanEdgeProcessor(metaClient);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (dataIterator == null && responseIterator == null && !scanPartIterator.hasNext()) {
+            return false;
+        }
+
+        while (dataIterator == null || !dataIterator.hasNext()) {
+            if (responseIterator == null || !responseIterator.hasNext()) {
+                if (scanPartIterator.hasNext()) {
+                    try {
+                        responseIterator = storageClient.scanEdge(connectInfo.getSpaceName(),
+                                scanPartIterator.next(), returnCols,
+                                false, 1000, 0L, Long.MAX_VALUE);
+                    } catch (IOException e) {
+                        logger.error(e.getMessage(), e);
+                    }
+                    continue;
+                }
+                break;
+            } else if (responseIterator.hasNext()) {
+                ScanEdgeResponse next = responseIterator.next();
+                if (next != null) {
+                    Result processResult = processor.process(connectInfo.getSpaceName(), next);
+                    dataIterator = process(processResult);
+                }
+                continue;
+            }
+        }
+        if (dataIterator == null) {
+            return false;
+        }
+        return dataIterator.hasNext();
+    }
+
+    @Override
+    protected Iterator<String[]> process(Result result) {
+        List<String[]> resultValues = new ArrayList<>();
+
+        Map<String, List<com.vesoft.nebula.data.Row>> dataMap = result.getRows();
+        for (Map.Entry<String, List<com.vesoft.nebula.data.Row>> dataEntry : dataMap.entrySet()) {
+            String labelName = dataEntry.getKey();
+            List<Integer> propIndexs = labelPropIndexMap.get(labelName);
+            List<com.vesoft.nebula.data.Row> rows = dataEntry.getValue();
+            for (com.vesoft.nebula.data.Row row : rows) {
+                Iterator<Integer> nameIndexIterator = propIndexs.iterator();
+                String[] fields = new String[propSize + 2];
+                fields[0] = String.valueOf(row.getDefaultProperties()[0].getValue());
+                fields[1] = String.valueOf(row.getDefaultProperties()[2].getValue());
+                Property[] properties = row.getProperties();
+                for (int i = 0; i < properties.length; i++) {
+                    fields[nameIndexIterator.next()] = String.valueOf(properties[i].getValue());
+                }
+                resultValues.add(fields);
+            }
+        }
+        return resultValues.iterator();
+    }
+}
