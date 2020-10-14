703d18cd86ebe5cf51ddbf1a21f0dabfb2247803
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2020-10-14 16:40:54 +0800

    spark-connector scala add converter: row -> InternalRow

--- tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/NebulaPartition.scala
+++ tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/NebulaPartition.scala
@@ -6,19 +6,22 @@
 
 package com.vesoft.nebula.tools.connector
 
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.sources.v2.reader.{InputPartition, InputPartitionReader}
+
 import scala.collection.mutable.ListBuffer
 
-//class NebulaVertexPartition(space: String, part: Int, tag: String, fields: String)
-//    extends InputPartition[InternalRow] {
-//  override def createPartitionReader(): InputPartitionReader[InternalRow] =
-//    new NebulaVertexPartitionReader(Nil, space, part, tag, fields.split(",").toList)
-//}
-//
-//class NebulaEdgePartition(space: String, part: Int, edge: String, fields: String)
-//    extends InputPartition[InternalRow] {
-//  override def createPartitionReader(): InputPartitionReader[InternalRow] =
-//    new NebulaEdgePartitionReader(Nil, space, part, edge, fields.split(",").toList)
-//}
+class NebulaVertexPartition(space: String, part: Int, tag: String, fields: String)
+    extends InputPartition[InternalRow] {
+  override def createPartitionReader(): InputPartitionReader[InternalRow] =
+    new NebulaVertexPartitionReader(Nil, space, part, tag, fields.split(",").toList)
+}
+
+class NebulaEdgePartition(space: String, part: Int, edge: String, fields: String)
+    extends InputPartition[InternalRow] {
+  override def createPartitionReader(): InputPartitionReader[InternalRow] =
+    new NebulaEdgePartitionReader(Nil, space, part, edge, fields.split(",").toList)
+}
 
 /* Copyright (c) 2020 vesoft inc. All rights reserved.
  *
@@ -28,24 +31,24 @@ import scala.collection.mutable.ListBuffer
 
 import org.apache.spark.Partition
 
-class NebulaPartition(var indexVar: Int) extends Partition {
-  override def index: Int = indexVar
-
-  /**
-    * allocate scanPart to partition
-    *
-    * @param totalPart nebula data part num
-    * @return scan data part list
-    */
-  def getScanParts(totalPart: Int, totalPartition: Int): List[Integer] = {
-    val scanParts: ListBuffer[Integer] = new ListBuffer[Integer]
-    var currentPart                    = index + 1
-    while ({
-      currentPart <= totalPart
-    }) {
-      scanParts.append(currentPart)
-      currentPart += totalPartition
-    }
-    scanParts.toList
-  }
-}
+//class NebulaPartition(var indexVar: Int) extends Partition {
+//  override def index: Int = indexVar
+//
+//  /**
+//    * allocate scanPart to partition
+//    *
+//    * @param totalPart nebula data part num
+//    * @return scan data part list
+//    */
+//  def getScanParts(totalPart: Int, totalPartition: Int): List[Integer] = {
+//    val scanParts: ListBuffer[Integer] = new ListBuffer[Integer]
+//    var currentPart                    = index + 1
+//    while ({
+//      currentPart <= totalPart
+//    }) {
+//      scanParts.append(currentPart)
+//      currentPart += totalPartition
+//    }
+//    scanParts.toList
+//  }
+//}
--- tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaIterator.scala
+++ tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaIterator.scala
@@ -11,31 +11,46 @@ import java.util
 import com.vesoft.nebula.client.meta.MetaClientImpl
 import com.vesoft.nebula.client.storage.StorageClientImpl
 import com.vesoft.nebula.client.storage.processor.Processor
-import com.vesoft.nebula.data.Result
-import com.vesoft.nebula.tools.connector.{NebulaOptions, NebulaPartition, NebulaUtils}
-import org.apache.spark
+import com.vesoft.nebula.data.{Property, Result}
+import com.vesoft.nebula.tools.connector.NebulaUtils.NebulaValueGetter
+import com.vesoft.nebula.tools.connector.{NebulaOptions, NebulaUtils}
+import org.apache.avro.generic.GenericData.StringType
 import org.apache.spark.Partition
-import org.apache.spark.sql.{Row, RowFactory, SparkSession}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.catalyst.expressions.SpecificInternalRow
+import org.apache.spark.sql.types.{
+  BooleanType,
+  CharType,
+  DataTypes,
+  DoubleType,
+  LongType,
+  StructType
+}
+import org.apache.spark.sql.{Row, RowFactory}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.{AbstractIterator, mutable}
 import scala.collection.JavaConverters._
 
-abstract class AbstractNebulaIterator extends AbstractIterator[Row] {
+abstract class AbstractNebulaIterator extends Iterator[InternalRow] {
   private val LOGGER: Logger = LoggerFactory.getLogger(classOf[AbstractNebulaIterator])
 
-  protected var dataIterator: Iterator[String]             = _
-  protected var scanPartIterator: Iterator[Integer]        = _
-  protected var resultValues: mutable.Map[String, List[_]] = mutable.Map[String, List[_]]()
+  protected var dataIterator: Iterator[String]      = _
+  protected var scanPartIterator: Iterator[Integer] = _
 
-  protected var storageClient: StorageClientImpl = _
-  protected var metaClient: MetaClientImpl       = _
-  protected var processor: Processor[_]          = _
-
+  protected var resultValues: mutable.Map[String, List[Property]] =
+    mutable.Map[String, List[Property]]()
+  protected var storageClient: StorageClientImpl                = _
+  protected var metaClient: MetaClientImpl                      = _
+  protected var processor: Processor[_]                         = _
   protected var returnCols: util.Map[String, util.List[String]] = new util.HashMap()
 
-  def this(split: Partition, nebulaOptions: NebulaOptions) {
+  private var schema: StructType = _
+
+  def this(split: Partition, nebulaOptions: NebulaOptions, schema: StructType) {
     this()
+    this.schema = schema
 
     nebulaOptions.getReturnColMap
       .foreach(entry => {
@@ -55,8 +70,16 @@ abstract class AbstractNebulaIterator extends AbstractIterator[Row] {
 
   override def hasNext: Boolean
 
-  override def next(): Row = {
-    RowFactory.create(resultValues.get(dataIterator.next()).toArray)
+  override def next(): InternalRow = {
+    val getters: Array[NebulaValueGetter] = NebulaUtils.makeGetters(schema)
+    val mutableRow                        = new SpecificInternalRow(schema.fields.map(x => x.dataType))
+
+    val resultSet: Array[Property] = resultValues(dataIterator.next()).toArray
+    for (i <- getters.indices) {
+      getters(i).apply(resultSet(i), mutableRow, i)
+      if (resultSet(i) == null) mutableRow.setNullAt(i)
+    }
+    mutableRow
   }
 
   protected def process(result: Result[com.vesoft.nebula.data.Row]): Iterator[String]
