1420701cbfeed2b49e8343fdccd2e7f2bedb83a6
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2020-10-21 14:35:08 +0800

    simply loop code

--- tools/exchange/src/main/scala/com/vesoft/nebula/tools/importer/config/Configs.scala
+++ tools/exchange/src/main/scala/com/vesoft/nebula/tools/importer/config/Configs.scala
@@ -544,7 +544,7 @@ object Configs {
                                 options)
       case SourceCategory.HBASE =>
         val fields: ListBuffer[String] = new ListBuffer[String]
-        fields.append(config.getStringList("fields").asScala.map(field => field): _*)
+        fields.append(config.getStringList("fields").asScala: _*)
 
         if (config.hasPath("vertex")) {
           fields.append(config.getString("vertex"))
