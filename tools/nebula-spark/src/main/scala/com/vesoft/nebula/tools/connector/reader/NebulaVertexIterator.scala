144a38e8c303a14c7947949a9f27442a21fcc82f
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2020-10-22 17:58:52 +0800

    delete useless code and correct comment

--- tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaVertexIterator.scala
+++ tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaVertexIterator.scala
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory
 import scala.collection.mutable.ListBuffer
 
 /**
-  * iterator to read nebula's vertex edge
+  * iterator to read nebula's vertex data
   */
 class NebulaVertexIterator(split: Partition, nebulaOptions: NebulaOptions, schema: StructType)
     extends AbstractNebulaIterator(split, nebulaOptions, schema) {
