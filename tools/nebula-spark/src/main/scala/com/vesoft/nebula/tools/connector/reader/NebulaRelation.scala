3554db67e6595f42287e3e547eeef1fb7735fef9
ericxluo <649555148@qq.com>
2020-12-08 12:08:10 +0800

    fix spark connector columns order mistaken

--- tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaRelation.scala
+++ tools/nebula-spark/src/main/scala/com/vesoft/nebula/tools/connector/reader/NebulaRelation.scala
@@ -7,9 +7,11 @@
 package com.vesoft.nebula.tools.connector.reader
 
 import com.vesoft.nebula.tools.connector.{DataTypeEnum, NebulaOptions, NebulaUtils}
+import com.vesoft.nebula.ColumnDef
+import com.vesoft.nebula.utils.NebulaTypeUtil
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.sources.{BaseRelation, TableScan}
-import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
+import org.apache.spark.sql.types.{DataType, DataTypes, StructField, StructType}
 import org.apache.spark.sql.{Row, SQLContext}
 import org.slf4j.LoggerFactory
 
@@ -30,6 +32,13 @@ private case class NebulaRelation(override val sqlContext: SQLContext, nebulaOpt
   override def schema: StructType = getSchema(nebulaOptions)
 
   /**
+    * convert [[ColumnDef]] to spark [[DataType]]
+    */
+  private def columnDef2dataType(columnDef: ColumnDef): DataType = {
+    NebulaUtils.convertDataType(NebulaTypeUtil.supportedTypeToClass(columnDef.getType.getType))
+  }
+
+  /**
     * return the dataset's schema. Schema includes configured cols in returnCols or includes all properties in nebula.
     */
   def getSchema(nebulaOptions: NebulaOptions): StructType = {
@@ -38,40 +47,34 @@ private case class NebulaRelation(override val sqlContext: SQLContext, nebulaOpt
     val metaClient                      = NebulaUtils.createMetaClient(nebulaOptions.getHostAndPorts, nebulaOptions)
 
     import scala.collection.JavaConverters._
-    var schemaColAndType: mutable.Map[String, Class[_]] = mutable.Map[String, Class[_]]()
+    var schemaCols: Seq[ColumnDef] = Seq()
 
     returnColMap.keySet.foreach(k => {
-      if (DataTypeEnum.VERTEX.toString.equalsIgnoreCase(nebulaOptions.dataType)) {
+      val schema = if (DataTypeEnum.VERTEX.toString.equalsIgnoreCase(nebulaOptions.dataType)) {
         fields.append(DataTypes.createStructField("_vertexId", DataTypes.StringType, false))
-        schemaColAndType =
-          metaClient.getTagSchema(nebulaOptions.spaceName, nebulaOptions.label).asScala
+        metaClient.getTag(nebulaOptions.spaceName, nebulaOptions.label)
       } else {
         fields.append(DataTypes.createStructField("_srcId", DataTypes.StringType, false))
         fields.append(DataTypes.createStructField("_dstId", DataTypes.StringType, false))
-        schemaColAndType =
-          metaClient.getEdgeSchema(nebulaOptions.spaceName, nebulaOptions.label).asScala
+        metaClient.getEdge(nebulaOptions.spaceName, nebulaOptions.label)
       }
+      schemaCols = schema.columns.asScala
       if (nebulaOptions.allCols) {
         // if allCols is true, then fields should contain all properties.
-        schemaColAndType.keySet
-          .foreach(propName => {
-            LOG.info(s"prop name ${propName}, type ${schemaColAndType} ")
-            fields.append(
-              DataTypes.createStructField(propName,
-                                          NebulaUtils.convertDataType(schemaColAndType(propName)),
-                                          true))
+        schemaCols
+          .foreach(columnDef => {
+            LOG.info(s"prop name ${columnDef.getName}, type ${columnDef.getType}")
+            fields.append(DataTypes.createStructField(columnDef.getName, columnDef2dataType(columnDef), true))
           })
       } else {
+        // create map, columnName -> columnDef
+        val colName2Def = schemaCols.groupBy(_.getName).mapValues(_.last)
         returnColMap(k)
           .foreach(returnCol => {
-            if (schemaColAndType.contains(returnCol)) {
-              fields.append(
-                DataTypes.createStructField(
-                  returnCol,
-                  NebulaUtils.convertDataType(schemaColAndType(returnCol)),
-                  true))
-            } else {
-              LOG.warn(s"label ${nebulaOptions.label} doesn't contain col $returnCol")
+            colName2Def.get(returnCol) match {
+              case Some(columnDef) =>
+                fields.append(DataTypes.createStructField(returnCol, columnDef2dataType(columnDef), true))
+              case None => LOG.warn(s"label ${nebulaOptions.label} doesn't contain col $returnCol")
             }
           })
       }
