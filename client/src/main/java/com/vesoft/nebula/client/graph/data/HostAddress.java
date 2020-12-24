8cb40e8e460a25b859c8fc290206863d9a2f9184
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2020-12-24 09:51:17 +0800

    add hashCode & equals method for HostAddress

--- client/src/main/java/com/vesoft/nebula/client/graph/data/HostAddress.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/data/HostAddress.java
@@ -7,8 +7,8 @@
 package com.vesoft.nebula.client.graph.data;
 
 public class HostAddress {
-    private String host;
-    private int port;
+    private final String host;
+    private final int port;
 
     public HostAddress(String host, int port) {
         this.host = host;
@@ -24,7 +24,24 @@ public class HostAddress {
     }
 
     @Override
+    public int hashCode() {
+        return host.hashCode() + port;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof HostAddress) {
+            HostAddress that = (HostAddress) obj;
+            return this.host.equals(that.host) && this.port == that.port;
+        }
+        return false;
+    }
+
+    @Override
     public String toString() {
-        return host +  ":" + port;
+        return host + ":" + port;
     }
 }
