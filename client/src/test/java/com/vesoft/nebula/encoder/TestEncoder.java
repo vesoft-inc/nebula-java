4bf280581183291b7cb2ff6426ee32f26989228e
laura-ding <48548375+laura-ding@users.noreply.github.com>
2021-03-11 21:52:53 +0800

    update encoder interfaces

--- client/src/test/java/com/vesoft/nebula/encoder/TestEncoder.java
+++ client/src/test/java/com/vesoft/nebula/encoder/TestEncoder.java
@@ -8,21 +8,29 @@ package test.java.com.vesoft.nebula.encoder;
 
 import com.vesoft.nebula.Date;
 import com.vesoft.nebula.DateTime;
+import com.vesoft.nebula.HostAddr;
 import com.vesoft.nebula.NullType;
 import com.vesoft.nebula.Time;
 import com.vesoft.nebula.Value;
 import com.vesoft.nebula.encoder.MetaCacheImplTest;
 import com.vesoft.nebula.encoder.NebulaCodecImpl;
-import java.io.File;
-import java.io.FileOutputStream;
+import com.vesoft.nebula.meta.EdgeItem;
+import com.vesoft.nebula.meta.PropertyType;
+import com.vesoft.nebula.meta.SpaceItem;
+import com.vesoft.nebula.meta.TagItem;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.MurmurHash2;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestEncoder {
+    private final MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
+    private final NebulaCodecImpl codec = new NebulaCodecImpl();
+
     final String expectResult = "090cc001081000200000004000000000000000db0f494069"
         + "57148b0abf05405d0000000c0000004e6562756c61204772617068bb334e5e000000"
         + "00e40702140a1e2d00000000e40702140a1e2d00000000000000000000000000000000"
@@ -57,12 +65,69 @@ public class TestEncoder {
             timestampVal, dateValue, timeVal, datetimeValue, nullVal);
     }
 
+    public int getSpaceVidLen(String spaceName) throws RuntimeException {
+        SpaceItem spaceItem = cacheImplTest.getSpace(spaceName);
+        if (spaceItem == null) {
+            throw new RuntimeException("SpaceName: " + spaceName + "is not existed");
+        }
+        if (spaceItem.properties.vid_type.type != PropertyType.FIXED_STRING) {
+            throw new RuntimeException("Only supported fixed string vid type.");
+        }
+        return spaceItem.properties.vid_type.type_length;
+    }
+
+    public int getPartSize(String spaceName) throws RuntimeException {
+        Map<Integer, List<HostAddr>> partsAlloc = cacheImplTest.getPartsAlloc(spaceName);
+        if (partsAlloc == null) {
+            throw new RuntimeException("SpaceName: " + spaceName + " is not existed");
+        }
+        return partsAlloc.size();
+    }
+
+    public int getPartId(int partNum, String vertexId) {
+        long hash = MurmurHash2.hash64(vertexId.getBytes(), vertexId.length(), 0xc70f6907);
+        long hashValue = Long.parseUnsignedLong(Long.toUnsignedString(hash));
+        return (int) (Math.floorMod(hashValue, partNum) + 1);
+    }
+
+    private byte[] getVertexKey(String spaceName, String vid, String tagName) {
+        int tagId = cacheImplTest.getTag(spaceName, tagName).tag_id;
+        return codec.vertexKey(getSpaceVidLen(spaceName),
+            getPartId(getPartSize(spaceName), vid), vid.getBytes(), tagId);
+    }
+
+    private byte[] getInBoundEdgeKey(String spaceName,
+                                     String edgeName,
+                                     String srcId,
+                                     String dstId,
+                                     Long ranking) {
+        int edgeType = cacheImplTest.getEdge(spaceName, edgeName).edge_type;
+        return codec.edgeKeyByDefaultVer(getSpaceVidLen(spaceName),
+            getPartId(getPartSize(spaceName), srcId),
+            srcId.getBytes(),
+            edgeType,
+            ranking,
+            dstId.getBytes());
+    }
+
+    private byte[] getOutBoundEdgeKey(String spaceName,
+                                      String edgeName,
+                                      String srcId,
+                                      String dstId,
+                                      Long ranking) {
+        int edgeType = cacheImplTest.getEdge(spaceName, edgeName).edge_type;
+        return codec.edgeKeyByDefaultVer(getSpaceVidLen(spaceName),
+            getPartId(getPartSize(spaceName), dstId),
+            dstId.getBytes(),
+            -edgeType,
+            ranking,
+            srcId.getBytes());
+    }
+
     @Test()
     public void testVertexKey() {
-        MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
-        NebulaCodecImpl codec = new NebulaCodecImpl(cacheImplTest);
         // test vertexKey
-        byte[] vertexKey = codec.vertexKey("test_space","Tom", "person");
+        byte[] vertexKey = getVertexKey("test_space","Tom", "person");
         String hexStr = Hex.encodeHexString(vertexKey);
         String expectResult = "01d80100546f6d00000000000000000000000000"
             + "0000000002000000";
@@ -70,13 +135,13 @@ public class TestEncoder {
 
         // less than vidLen
         String vid = "0123456789";
-        vertexKey = codec.genVertexKey(10,123, vid.getBytes(), 2020);
+        vertexKey = codec.vertexKey(10,123, vid.getBytes(), 2020);
         hexStr = Hex.encodeHexString(vertexKey);
         expectResult = "017b000030313233343536373839e4070000";
         Assert.assertArrayEquals(expectResult.getBytes(), hexStr.getBytes());
 
         // large than vidLen
-        vertexKey = codec.genVertexKey(100,123, vid.getBytes(), 2020);
+        vertexKey = codec.vertexKey(100,123, vid.getBytes(), 2020);
         hexStr = Hex.encodeHexString(vertexKey);
         expectResult = "017b00003031323334353637383900000000000000000000000000000000000"
             + "00000000000000000000000000000000000000000000000000000000000000000000000000"
@@ -87,11 +152,8 @@ public class TestEncoder {
 
     @Test()
     public void testEdgeKey() {
-        MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
-        NebulaCodecImpl codec = new NebulaCodecImpl(cacheImplTest);
-
         // test edgeKey
-        byte[] edgeKey = codec.edgeKey("test_space","Kate", "friend", 0, "Lily");
+        byte[] edgeKey = getInBoundEdgeKey("test_space", "friend", "Kate","Lily", 0L);
         String hexStr = Hex.encodeHexString(edgeKey);
         String expectResult = "02ef00004b617465000000000000000000000000000000000300000080000"
             + "000000000004c696c790000000000000000000000000000000001";
@@ -101,14 +163,14 @@ public class TestEncoder {
         String dstVid = "9876543210";
 
         // less than vidLen
-        edgeKey = codec.genEdgeKeyByDefaultVer(
+        edgeKey = codec.edgeKeyByDefaultVer(
             10,123, srcVid.getBytes(), 1010, 10L, dstVid.getBytes());
         hexStr = Hex.encodeHexString(edgeKey);
         expectResult = "027b000030313233343536373839f2030000800000000000000a3938373635343332313001";
         Assert.assertArrayEquals(expectResult.getBytes(), hexStr.getBytes());
 
         // large than vidLen
-        edgeKey = codec.genEdgeKeyByDefaultVer(
+        edgeKey = codec.edgeKeyByDefaultVer(
             100,123, srcVid.getBytes(), 1010, 10L, dstVid.getBytes());
         hexStr = Hex.encodeHexString(edgeKey);
         expectResult = "027b0000303132333435363738390000000000000000000000000000000000000000"
@@ -120,7 +182,7 @@ public class TestEncoder {
         Assert.assertArrayEquals(expectResult.getBytes(), hexStr.getBytes());
 
         // less than vidLen with negative edgeType
-        edgeKey = codec.genEdgeKeyByDefaultVer(
+        edgeKey = codec.edgeKeyByDefaultVer(
             10,123, srcVid.getBytes(), -1010, 10L, dstVid.getBytes());
         hexStr = Hex.encodeHexString(edgeKey);
         expectResult = "027b0000303132333435363738390efcffff800000000000000a39"
@@ -128,7 +190,7 @@ public class TestEncoder {
         Assert.assertArrayEquals(expectResult.getBytes(), hexStr.getBytes());
 
         // large than vidLen with negative edgeType
-        edgeKey = codec.genEdgeKeyByDefaultVer(
+        edgeKey = codec.edgeKeyByDefaultVer(
             100,123, srcVid.getBytes(), -1010, 10L, dstVid.getBytes());
         hexStr = Hex.encodeHexString(edgeKey);
         expectResult = "027b0000303132333435363738390000000000000000000000000000000000000000000"
@@ -142,13 +204,14 @@ public class TestEncoder {
 
     @Test()
     public void testEncodeVertexValue() {
-        MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
-        NebulaCodecImpl codec = new NebulaCodecImpl(cacheImplTest);
         // encode failed, loss filed value
         List<String> colNames = Arrays.asList("Col01", "Col02", "Col03", "Col04", "Col05");
         List<Object> colVals = Arrays.asList(true, 8, 16, 32, (long)100);
+        TagItem tagItem1 = cacheImplTest.getTag("test", "tag_no_default");
+        TagItem tagItem2 = cacheImplTest.getTag("test", "tag_with_empty_string");
+        TagItem tagItem3 = cacheImplTest.getTag("test", "tag_with_default");
         try {
-            codec.encodeTag("test","tag_no_default", colNames, colVals);
+            codec.encodeTag(tagItem1, colNames, colVals);
             Assert.fail();
         } catch (Exception exception) {
             assert (true);
@@ -156,7 +219,7 @@ public class TestEncoder {
 
         // encode all type succeeded
         try {
-            byte[] encodeStr = codec.encodeTag("test", "tag_no_default", getCols(), getValues());
+            byte[] encodeStr = codec.encodeTag(tagItem1, getCols(), getValues());
             String hexStr = Hex.encodeHexString(encodeStr);
             // write into file to use storage test to decode
             // File file = new File("encode_java.txt");
@@ -170,7 +233,7 @@ public class TestEncoder {
 
         // encode empty string succeeded
         try {
-            byte[] encodeStr = codec.encodeTag("test", "tag_with_empty_string",
+            byte[] encodeStr = codec.encodeTag(tagItem2,
                 Collections.singletonList("Col01"), Collections.singletonList(""));
             String hexStr = Hex.encodeHexString(encodeStr);
             String expectResult = "080900000000000000";
@@ -182,10 +245,7 @@ public class TestEncoder {
 
         // test with default
         try {
-            codec.encodeTag("test",
-                "tag_with_default",
-                Arrays.asList("Col01", "Col02"),
-                Arrays.asList(true, 8));
+            codec.encodeTag(tagItem3, Arrays.asList("Col01", "Col02"), Arrays.asList(true, 8));
         } catch (RuntimeException e) {
             Assert.assertArrayEquals(e.getMessage().getBytes(),
                 "Unsupported default value yet".getBytes());
@@ -195,13 +255,13 @@ public class TestEncoder {
 
     @Test()
     public void testEncodeEdge() {
-        MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
-        NebulaCodecImpl codec = new NebulaCodecImpl(cacheImplTest);
         // encode failed, loss filed value
         List<String> colNames = Arrays.asList("Col01", "Col02", "Col03", "Col04", "Col05");
         List<Object> colVals = Arrays.asList(true, 8, 16, 32, (long)100);
+        EdgeItem edgeItem1 = cacheImplTest.getEdge("test", "edge_no_default");
+        EdgeItem edgeItem2 = cacheImplTest.getEdge("test", "edge_with_empty_string");
         try {
-            codec.encodeEdge("test", "edge_no_default", colNames, colVals);
+            codec.encodeEdge(edgeItem1, colNames, colVals);
             Assert.fail();
         } catch (Exception exception) {
             assert (true);
@@ -209,7 +269,7 @@ public class TestEncoder {
 
         // encode succeeded
         try {
-            byte[] encodeStr = codec.encodeEdge("test", "edge_no_default", getCols(), getValues());
+            byte[] encodeStr = codec.encodeEdge(edgeItem1, getCols(), getValues());
             String hexStr = Hex.encodeHexString(encodeStr);
             Assert.assertArrayEquals(expectResult.getBytes(), hexStr.getBytes());
         } catch (Exception exception) {
@@ -218,7 +278,7 @@ public class TestEncoder {
         }
         // encode empty string succeeded
         try {
-            byte[] encodeStr = codec.encodeEdge("test", "edge_with_empty_string",
+            byte[] encodeStr = codec.encodeEdge(edgeItem2,
                 Collections.singletonList("Col01"), Collections.singletonList(""));
             String hexStr = Hex.encodeHexString(encodeStr);
             String expectResult = "080900000000000000";
@@ -231,25 +291,23 @@ public class TestEncoder {
 
     @Test()
     public void testEncodeWithNotExistedSchema() {
-        MetaCacheImplTest cacheImplTest = new MetaCacheImplTest();
-        NebulaCodecImpl codec = new NebulaCodecImpl(cacheImplTest);
         List<String> colNames = Arrays.asList("Col01", "Col02", "Col03", "Col04", "Col05");
         List<Object> colVals = Arrays.asList(true, 8, 16, 32, (long) 100);
         try {
-            codec.encodeTag("test", "not_existed", colNames, colVals);
+            codec.encodeTag(null, colNames, colVals);
             Assert.fail();
         } catch (RuntimeException e) {
             Assert.assertArrayEquals(e.getMessage().getBytes(),
-                "Tag: not_existed does not exist.".getBytes());
+                "TagItem is null".getBytes());
             assert (true);
         }
 
         try {
-            codec.encodeEdge("test", "not_existed", colNames, colVals);
+            codec.encodeEdge(null, colNames, colVals);
             Assert.fail();
         } catch (RuntimeException e) {
             Assert.assertArrayEquals(e.getMessage().getBytes(),
-                "Edge: not_existed does not exist.".getBytes());
+                "EdgeItem is null".getBytes());
             assert (true);
         }
     }
