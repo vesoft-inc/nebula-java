78d75ded3e678951866f9702db9761f103759c67
Nicole00 <16240361+Nicole00@users.noreply.github.com>
2021-07-15 16:36:50 +0800

    extract printProcessStatus function as util

--- client/src/test/java/com/vesoft/nebula/client/util/ProcessUtil.java
+++ client/src/test/java/com/vesoft/nebula/client/util/ProcessUtil.java
@@ -3,6 +3,26 @@
  * This source code is licensed under Apache 2.0 License,
  * attached with Common Clause Condition 1.0, found in the LICENSES directory.
  */
- 
-package com.vesoft.nebula.client.util;public class ProcessUtil {
+
+package com.vesoft.nebula.client.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ProcessUtil {
+    public static void printProcessStatus(String cmd, Process p) {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(p.getInputStream()));
+
+            String line;
+            System.out.print(cmd + " output: ");
+            while ((line = reader.readLine()) != null) {
+                System.out.print(line);
+            }
+            System.out.print("\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
