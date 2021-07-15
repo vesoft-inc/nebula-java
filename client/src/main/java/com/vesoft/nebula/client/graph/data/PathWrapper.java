141cb4855195a3aee9017e90a7c803f79b0e72af
laura-ding <48548375+laura-ding@users.noreply.github.com>
2021-07-15 17:01:09 +0800

    fix the toString of path (#327)

--- client/src/main/java/com/vesoft/nebula/client/graph/data/PathWrapper.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/data/PathWrapper.java
@@ -21,6 +21,7 @@ public class PathWrapper {
     private List<Segment> segments = new ArrayList<>();
     private List<Node> nodes = new ArrayList<>();
     private List<Relationship> relationships = new ArrayList<>();
+    private Path path = null;
 
     public static class Segment {
         Node startNode;
@@ -190,48 +191,28 @@ public class PathWrapper {
     @Override
     public String toString() {
         try {
-            Node startNode = getStartNode();
             List<String> edgeStrs = new ArrayList<>();
-            if (segments.size() >= 1) {
+            for (int i = 0; i < relationships.size(); i++) {
+                Relationship relationship = relationships.get(i);
                 List<String> propStrs = new ArrayList<>();
-                Map<String, ValueWrapper> props = segments.get(0).getRelationShip().properties();
+                Map<String, ValueWrapper> props = relationship.properties();
                 for (String key : props.keySet()) {
                     propStrs.add(key + ":" + props.get(key).toString());
                 }
-                if (segments.get(0).getStartNode() == startNode) {
+                Step step = path.steps.get(i);
+                Node node = (Node) new Node(step.dst);
+                if (step.type > 0) {
                     edgeStrs.add(String.format("-[:%s@%d{%s}]->%s",
-                        segments.get(0).getRelationShip().edgeName(),
-                        segments.get(0).getRelationShip().ranking(),
+                        relationship.edgeName(),
+                        relationship.ranking(),
                         String.join(", ", propStrs),
-                        segments.get(0).getEndNode().toString()));
+                        node.toString()));
                 } else {
                     edgeStrs.add(String.format("<-[:%s@%d{%s}]-%s",
-                        segments.get(0).getRelationShip().edgeName(),
-                        segments.get(0).getRelationShip().ranking(),
+                        relationship.edgeName(),
+                        relationship.ranking(),
                         String.join(", ", propStrs),
-                        segments.get(0).getStartNode().toString()));
-                }
-
-            }
-
-            for (int i = 1; i < segments.size(); i++) {
-                List<String> propStrs = new ArrayList<>();
-                Map<String, ValueWrapper> props = segments.get(0).getRelationShip().properties();
-                for (String key : props.keySet()) {
-                    propStrs.add(key + ":" + props.get(key).toString());
-                }
-                if (segments.get(i).getStartNode() == segments.get(i - 1).getStartNode()) {
-                    edgeStrs.add(String.format("-[:%s@%d{%s}]->%s",
-                        segments.get(i).getRelationShip().edgeName(),
-                        segments.get(i).getRelationShip().ranking(),
-                        String.join(", ", propStrs),
-                        segments.get(i).getEndNode().toString()));
-                } else {
-                    edgeStrs.add(String.format("<-[:%s@%d{%s}]-%s",
-                        segments.get(i).getRelationShip().edgeName(),
-                        segments.get(i).getRelationShip().ranking(),
-                        String.join(", ", propStrs),
-                        segments.get(i).getStartNode().toString()));
+                        node.toString()));
                 }
             }
             return String.format("%s%s",
