3cdfc75fa61a8002b07db883eb0c874af531bd53
laura-ding <48548375+laura-ding@users.noreply.github.com>
2021-08-06 14:15:39 +0800

    fix timeout problem

--- client/src/main/java/com/vesoft/nebula/client/graph/net/NebulaPool.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/net/NebulaPool.java
@@ -88,16 +88,18 @@ public class NebulaPool {
 
     public Session getSession(String userName, String password, boolean reconnect)
             throws NotValidConnectionException, IOErrorException, AuthFailedException {
+        SyncConnection connection = null;
         try {
-            SyncConnection connection = getConnection();
+            connection = getConnection();
             long sessionID = connection.authenticate(userName, password);
             return new Session(connection, sessionID, this, reconnect);
-        } catch (NotValidConnectionException | AuthFailedException | IOErrorException e) {
+        } catch (AuthFailedException | IOErrorException e) {
+            // if get the connection succeeded, but authenticate failed,
+            // needs to return connection to pool
+            if (connection != null) {
+                setInvalidateConnection(connection);
+            }
             throw e;
-        } catch (IllegalStateException e) {
-            throw new NotValidConnectionException(e.getMessage());
-        } catch (Exception e) {
-            throw new IOErrorException(IOErrorException.E_UNKNOWN, e.getMessage());
         }
     }
 
@@ -131,49 +133,11 @@ public class NebulaPool {
         objectPool.returnObject(connection);
     }
 
-    protected SyncConnection getConnection() throws NotValidConnectionException, IOErrorException {
-        // If no idle connection, try once
-        int idleConnNum = getIdleConnNum();
-        int retry = idleConnNum == 0 ? 1 : idleConnNum;
-        SyncConnection connection = null;
-        boolean hasOkConn = false;
+    protected SyncConnection getConnection() throws NotValidConnectionException {
         try {
-            while (retry-- > 0) {
-                connection = objectPool.borrowObject(waitTime);
-                if (connection == null) {
-                    continue;
-                }
-                if (!connection.ping()) {
-                    log.info("The connection is broken, set invalidateObject");
-                    setInvalidateConnection(connection);
-                    continue;
-                }
-                hasOkConn = true;
-                break;
-            }
-            // All idle connections are broken, so need to create new one
-            if (!hasOkConn) {
-                connection = objectPool.borrowObject(waitTime);
-                if (connection == null) {
-                    throw new NotValidConnectionException("Get null connection from the pool");
-                }
-                if (!connection.ping()) {
-                    log.info("The connection is broken, set invalidateObject");
-                    setInvalidateConnection(connection);
-                    throw new NotValidConnectionException("The connection ping failed.");
-                }
-                log.info("Create new connection");
-            }
-            log.info(String.format("Get connection to %s:%d",
-                    connection.getServerAddress().getHost(),
-                    connection.getServerAddress().getPort()));
-            return connection;
-        } catch (NotValidConnectionException | IOErrorException e) {
-            throw e;
-        } catch (IllegalStateException e) {
-            throw new NotValidConnectionException(e.getMessage());
+            return objectPool.borrowObject(waitTime);
         } catch (Exception e) {
-            throw new IOErrorException(IOErrorException.E_UNKNOWN, e.getMessage());
+            throw new NotValidConnectionException(e.getMessage());
         }
     }
 }
--- client/src/main/java/com/vesoft/nebula/client/graph/net/SyncConnection.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/net/SyncConnection.java
@@ -25,14 +25,15 @@ public class SyncConnection extends Connection {
     protected TTransport transport = null;
     protected TProtocol protocol = null;
     private GraphService.Client client = null;
+    private int timeout = 0;
 
     @Override
     public void open(HostAddress address, int timeout) throws IOErrorException {
         this.serverAddr = address;
         try {
-            int newTimeout = timeout <= 0 ? Integer.MAX_VALUE : timeout;
+            this.timeout  = timeout <= 0 ? Integer.MAX_VALUE : timeout;
             this.transport = new TSocket(
-                    address.getHost(), address.getPort(), newTimeout, newTimeout);
+                    address.getHost(), address.getPort(), this.timeout, this.timeout);
             this.transport.open();
             this.protocol = new TCompactProtocol(transport);
             client = new GraphService.Client(protocol);
@@ -41,12 +42,24 @@ public class SyncConnection extends Connection {
         }
     }
 
+    @Override
+    public void reopen() throws IOErrorException {
+        close();
+        open(serverAddr, timeout);
+    }
+
     public long authenticate(String user, String password)
             throws AuthFailedException, IOErrorException {
         try {
             AuthResponse resp = client.authenticate(user.getBytes(), password.getBytes());
             if (resp.error_code != ErrorCode.SUCCEEDED) {
-                throw new AuthFailedException(new String(resp.error_msg).intern());
+                if (resp.error_msg != null) {
+                    throw new AuthFailedException(new String(resp.error_msg));
+                } else {
+                    throw new AuthFailedException(
+                        "The error_msg is null, "
+                            + "maybe the service not set or the response is disorder.");
+                }
             }
             return resp.session_id;
         } catch (TException e) {
@@ -54,7 +67,8 @@ public class SyncConnection extends Connection {
                 TTransportException te = (TTransportException)e;
                 if (te.getType() == TTransportException.END_OF_FILE) {
                     throw new IOErrorException(IOErrorException.E_CONNECT_BROKEN, te.getMessage());
-                } else if (te.getType() == TTransportException.TIMED_OUT) {
+                } else if (te.getType() == TTransportException.TIMED_OUT
+                    || te.getMessage().contains("Read timed out")) {
                     throw new IOErrorException(IOErrorException.E_TIME_OUT, te.getMessage());
                 } else if (te.getType() == TTransportException.NOT_OPEN) {
                     throw new IOErrorException(IOErrorException.E_NO_OPEN, te.getMessage());
@@ -73,6 +87,11 @@ public class SyncConnection extends Connection {
                 TTransportException te = (TTransportException) e;
                 if (te.getType() == TTransportException.END_OF_FILE) {
                     throw new IOErrorException(IOErrorException.E_CONNECT_BROKEN, te.getMessage());
+                } else if (te.getType() == TTransportException.NOT_OPEN) {
+                    throw new IOErrorException(IOErrorException.E_NO_OPEN, te.getMessage());
+                } else if (te.getType() == TTransportException.TIMED_OUT
+                    || te.getMessage().contains("Read timed out")) {
+                    throw new IOErrorException(IOErrorException.E_TIME_OUT, te.getMessage());
                 }
             }
             throw new IOErrorException(IOErrorException.E_UNKNOWN, e.getMessage());
