1b1a86c9d7cd322aca9fbdfb329fd99ac6bfa2bf
hebelala <hebelala@qq.com>
2021-09-03 16:48:20 +0800

    fix java.util.IllegalFormatConversionException

--- client/src/main/java/com/vesoft/nebula/client/storage/scan/ScanVertexResultIterator.java
+++ client/src/main/java/com/vesoft/nebula/client/storage/scan/ScanVertexResultIterator.java
@@ -178,7 +178,7 @@ public class ScanVertexResultIterator extends ScanResultIterator {
                 freshLeader(spaceName, partInfo.getPart(), partResult.getLeader());
                 partInfo.setLeader(getLeader(partResult.getLeader()));
             } else {
-                ErrorCode code = partResult.getCode();
+				int code = partResult.getCode().getValue();
                 LOGGER.error(String.format("part scan failed, error code=%d", code));
                 partScanQueue.dropPart(partInfo);
                 exceptions.add(new Exception(String.format("part scan, error code=%d", code)));
