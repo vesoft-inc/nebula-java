c6bf2d936aa23c72eb233df8464528d51eb65eed
Nicole00 <anqi.wang@vesoft.com>
2022-09-27 15:09:09 +0800

    add check for config

--- client/src/main/java/com/vesoft/nebula/client/graph/SessionPoolConfig.java
+++ client/src/main/java/com/vesoft/nebula/client/graph/SessionPoolConfig.java
@@ -63,128 +63,184 @@ public class SessionPoolConfig implements Serializable {
         return username;
     }
 
-    public void setUsername(String username) {
+    public SessionPoolConfig setUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new IllegalArgumentException("user name cannot be blank.");
+        }
         this.username = username;
+        return this;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
+    public SessionPoolConfig setPassword(String password) {
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("password cannot be blank.");
+        }
         this.password = password;
+        return this;
     }
 
     public List<HostAddress> getGraphAddressList() {
         return graphAddressList;
     }
 
-    public void setGraphAddressList(List<HostAddress> graphAddressList) {
+    public SessionPoolConfig setGraphAddressList(List<HostAddress> graphAddressList) {
+        if (graphAddressList == null || graphAddressList.size() == 0) {
+            throw new IllegalArgumentException("Graph addresses cannot be empty.");
+        }
         this.graphAddressList = graphAddressList;
+        return this;
     }
 
     public String getSpaceName() {
         return spaceName;
     }
 
-    public void setSpaceName(String spaceName) {
+    public SessionPoolConfig setSpaceName(String spaceName) {
+        if (spaceName == null || spaceName.trim().isEmpty()) {
+            throw new IllegalArgumentException("space name cannot be blank.");
+        }
         this.spaceName = spaceName;
+        return this;
     }
 
     public int getMinConnsSize() {
         return minConnsSize;
     }
 
-    public void setMinConnsSize(int minConnsSize) {
+    public SessionPoolConfig setMinConnsSize(int minConnsSize) {
+        if (minConnsSize < 0) {
+            throw new IllegalArgumentException("minConnsSize cannot be less than 0.");
+        }
         this.minConnsSize = minConnsSize;
+        return this;
     }
 
     public int getMaxConnsSize() {
         return maxConnsSize;
     }
 
-    public void setMaxConnsSize(int maxConnsSize) {
+    public SessionPoolConfig setMaxConnsSize(int maxConnsSize) {
+        if (maxConnsSize < 1) {
+            throw new IllegalArgumentException("maxConnsSize cannot be less than 1.");
+        }
         this.maxConnsSize = maxConnsSize;
+        return this;
     }
 
     public int getMinSessionSize() {
         return minSessionSize;
     }
 
-    public void setMinSessionSize(int minSessionSize) {
+    public SessionPoolConfig setMinSessionSize(int minSessionSize) {
+        if (minSessionSize < 0) {
+            throw new IllegalArgumentException("minSessionSize cannot be less than 0.");
+        }
         this.minSessionSize = minSessionSize;
+        return this;
     }
 
     public int getMaxSessionSize() {
         return maxSessionSize;
     }
 
-    public void setMaxSessionSize(int maxSessionSize) {
+    public SessionPoolConfig setMaxSessionSize(int maxSessionSize) {
+        if (maxSessionSize < 1) {
+            throw new IllegalArgumentException("maxSessionSize cannot be less than 1.");
+        }
         this.maxSessionSize = maxSessionSize;
+        return this;
     }
 
     public int getTimeout() {
         return timeout;
     }
 
-    public void setTimeout(int timeout) {
+    public SessionPoolConfig setTimeout(int timeout) {
+        if (timeout < 0) {
+            throw new IllegalArgumentException("timeout cannot be less than 0.");
+        }
         this.timeout = timeout;
+        return this;
     }
 
     public int getIdleTime() {
         return idleTime;
     }
 
-    public void setIdleTime(int idleTime) {
+    public SessionPoolConfig setIdleTime(int idleTime) {
+        if (idleTime < 0) {
+            throw new IllegalArgumentException("idleTime cannot be less than 0.");
+        }
         this.idleTime = idleTime;
+        return this;
     }
 
     public int getIntervalIdle() {
         return intervalIdle;
     }
 
-    public void setIntervalIdle(int intervalIdle) {
+    public SessionPoolConfig setIntervalIdle(int intervalIdle) {
+        if (intervalIdle < 0) {
+            throw new IllegalArgumentException("intervalIdle cannot be less than 0.");
+        }
         this.intervalIdle = intervalIdle;
+        return this;
     }
 
     public int getWaitTime() {
         return waitTime;
     }
 
-    public void setWaitTime(int waitTime) {
+    public SessionPoolConfig setWaitTime(int waitTime) {
+        if (waitTime < 0) {
+            throw new IllegalArgumentException("waitTime cannot be less than 0.");
+        }
         this.waitTime = waitTime;
+        return this;
     }
 
     public double getMinClusterHealthRate() {
         return minClusterHealthRate;
     }
 
-    public void setMinClusterHealthRate(double minClusterHealthRate) {
+    public SessionPoolConfig setMinClusterHealthRate(double minClusterHealthRate) {
+        if (minClusterHealthRate <= 0 || minClusterHealthRate > 1) {
+            throw new IllegalArgumentException(
+                    "minClusterHealthRate cannot be less than 0 or larger than 1.");
+        }
         this.minClusterHealthRate = minClusterHealthRate;
+        return this;
     }
 
     public boolean isEnableSsl() {
         return enableSsl;
     }
 
-    public void setEnableSsl(boolean enableSsl) {
+    public SessionPoolConfig setEnableSsl(boolean enableSsl) {
         this.enableSsl = enableSsl;
+        return this;
     }
 
     public SSLParam getSslParam() {
         return sslParam;
     }
 
-    public void setSslParam(SSLParam sslParam) {
+    public SessionPoolConfig setSslParam(SSLParam sslParam) {
         this.sslParam = sslParam;
+        return this;
     }
 
     public boolean isReConnect() {
         return reConnect;
     }
 
-    public void setReConnect(boolean reConnect) {
+    public SessionPoolConfig setReConnect(boolean reConnect) {
         this.reConnect = reConnect;
+        return this;
     }
 
     @Override
