09755fc4c599533129e1507db7fe6241a7d8ec50
Anqi <16240361+Nicole00@users.noreply.github.com>
2026-05-07 11:15:11 +0800

    update version to 5.3.0

--- client/src/main/java/com/vesoft/nebula/driver/graph/utils/ClientVersion.java
+++ client/src/main/java/com/vesoft/nebula/driver/graph/utils/ClientVersion.java
@@ -6,5 +6,5 @@
 package com.vesoft.nebula.driver.graph.utils;
 
 public class ClientVersion {
-    public static final String clientVersion = "5.3-SNAPSHOT";
+    public static final String clientVersion = "5.3.0";
 }
