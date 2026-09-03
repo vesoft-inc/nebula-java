9e366401a6a1e5445d0eecb16b4e67a95b958f64
Anqi <16240361+Nicole00@users.noreply.github.com>
2026-09-03 21:28:49 +0800

    cherrypick: Remove Fastjson from driver & update version (#657)
    * Remove Fastjson from the driver (#656)
    
    Replace Fastjson authentication option serialization with a local string-map JSON serializer and remove the Fastjson dependency. Keep the gRPC and Protobuf vulnerability updates in the same security change.
    
    * update version
    
    ---------
    
    Co-authored-by: jingchun.xia <6269380+xiajingchun@users.noreply.github.com>

--- client/src/main/java/com/vesoft/nebula/driver/graph/utils/ClientVersion.java
+++ client/src/main/java/com/vesoft/nebula/driver/graph/utils/ClientVersion.java
@@ -6,5 +6,5 @@
 package com.vesoft.nebula.driver.graph.utils;
 
 public class ClientVersion {
-    public static final String clientVersion = "5.3.0";
+    public static final String clientVersion = "5.3.1";
 }
